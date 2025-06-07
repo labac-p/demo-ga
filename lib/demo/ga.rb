f342227b0ea063eeb6ff5787708d9c0455b5462b
Bac La <100670919+labac-p@users.noreply.github.com>
2025-06-07 10:37:12 +0700

    feat: Hello

--- lib/demo/ga.rb
+++ lib/demo/ga.rb
@@ -1,10 +1,11 @@
-# frozen_string_literal: true
-
 require_relative "ga/version"
 
 module Demo
   module Ga
     class Error < StandardError; end
-    # Your code goes here...
+
+    def self.hello
+      "Hello, world!"
+    end
   end
 end
